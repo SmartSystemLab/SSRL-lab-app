05a77ef1f2a7243c9cbe370cd4ae26f7a911a80f
Agboola Oluwatofunmi <oluwatofunmiagboola1@gmail.com>
2024-06-17 05:41:35 -0700

    created login,signup,reset password pages

--- src/components/MyRoutes.jsx
+++ src/components/MyRoutes.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Test from './Test'
+import SharedLayouts from '../sharedLayouts/SharedLayouts'
+import Login from '../pages/Login'
+import ResetPassword from '../pages/ResetPassword'
+import ForgotPassword from '../pages/ForgotPassword'
 
 const MyRoutes = () => {
     return (
         <Router>
             <Routes>
-                <Route path='/test' element={<Test />} />
+                <Route path='/' element={<SharedLayouts />}>
+                    <Route index element={<Login />} />
+                    <Route path='/forgotpassword' element={<ForgotPassword />} />
+                    <Route path='/resetpassword' element={< ResetPassword />} />
+                </Route>
             </Routes>
         </Router>
     )
