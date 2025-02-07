2720d0c10dcf40efd117e8d565915d2a382df94a
dabirax <oliverpraise1@gmail.com>
2025-02-07 13:06:34 +0100

    to-do-1

--- src/components/BigGreenButton.jsx
+++ src/components/BigGreenButton.jsx
@@ -1,7 +1,7 @@
-const BigGreenButton = ({ children, action, type}) => {
+const BigGreenButton = ({ children, action, type, className}) => {
   return (
     <button
-      className="cursor-pointer rounded-full bg-navBg2 px-4 py-1 font-medium capitalize text-white hover:scale-105 w-fit"
+      className={`cursor-pointer rounded-full bg-navBg2 px-4 py-1 font-medium capitalize text-white hover:scale-105 w-fit ${className} `}
       onClick={action}
       type={type || "button"}
     >
