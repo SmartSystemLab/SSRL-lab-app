bfe14106bad77f6caeec7536d85a2ee28daa7884
Ceejay-777 <covenantcrackslord01@gmail.com>
2025-02-12 03:08:31 +0100

    Working on reports page

--- src/pages/reports/components/ListDisplay.jsx
+++ src/pages/reports/components/ListDisplay.jsx
@@ -1,17 +1,18 @@
-
-
 const ListDisplay = ({ Text, content }) => {
-    return (
-        <div>
+  return (
+    <div>
+      <div className="mb-1 rounded-xl bg-[#347831] p-2 text-lg text-white">
+        {Text}
+      </div>
+      <ul className="ml-2 mt-3 list-outside list-disc space-y-3 px-4 marker:text-navBg2">
+        {content.map((task, index) => (
+          <li key={index} className="break-words">
+            {task}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-            <div className="bg-[#347831] rounded-xl p-2 text-white text-lg mb-1">{Text}</div>
-            <ul className="list-disc list-outside ml-2 space-y-3 px-4 mt-3 marker:text-navBg2">
-                {content.map((task, index) => (
-                    <li key={index} className="break-words">{task}</li>
-                ))}
-            </ul>
-        </div>
-    )
-}
-
-export default ListDisplay
+export default ListDisplay;
--- src/pages/reports/pages/ViewReport.jsx
+++ src/pages/reports/pages/ViewReport.jsx
@@ -1,76 +1,80 @@
-import { useState } from "react"
-import { useLocation } from "react-router-dom"
-import Feedback from "../components/Feedback"
-import BigGreenButton from '../../../components/BigGreenButton'
-import ListDisplay from "../components/ListDisplay"
-
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Feedback from "../components/Feedback";
+import BigGreenButton from "../../../components/BigGreenButton";
+import ListDisplay from "../components/ListDisplay";
 
 const ViewReport = () => {
-    const location = useLocation()
-    const report = location.state
-    const [showPopup, setShowPopup] = useState(false)
-    const handleSendFeedback = (feedbackText) => {
+  const location = useLocation();
+  const report = location.state;
+  console.log(report);
+  const [showPopup, setShowPopup] = useState(false);
+  const handleSendFeedback = (feedbackText) => {
+    console.log("Feedback sent:", feedbackText);
+  };
+  return (
+    <div className="relative min-h-screen overflow-y-auto px-6 py-4">
+      <h2 className="text-2xl font-semibold capitalize">
+        {report.type} Report
+      </h2>
+      <hr className="mt-1 bg-black" />
+      <div className="mx-auto mt-10 flex flex-col gap-5 rounded-xl border px-10 py-8 shadow-lg">
+        <div className="flex flex-col items-start md:flex-row md:justify-between">
+          <div className="space-y-3">
+            <p className="flex gap-2">
+              <span className="font-medium">Intern Name:</span>
+              {report.sender.name}
+            </p>
+            <p className="flex gap-2">
+              <span className="font-medium">Stack:</span>
+              {report.stack}
+            </p>
+            {report.report_type === "activity" && (
+              <p className="flex gap-2">
+                <span className="font-medium">Duration:</span>
+                {report.duration}
+              </p>
+            )}
+          </div>
 
-        console.log('Feedback sent:', feedbackText);
-    };
-    return (
-        <div className="px-6 py-4 min-h-screen overflow-y-auto relative">
-            <h2 className="text-2xl font-semibold capitalize">{report.type} Report</h2>
-            <hr className="bg-black mt-1" />
-            <div className="mx-auto mt-10 flex flex-col gap-5 rounded-xl border px-10 py-8 shadow-lg">
-                <div className=" flex flex-col md:flex-row md:justify-between items-start">
-                    <div className="space-y-3">
-                        <p className="flex gap-2">
-                            <span className="font-medium">Intern Name:</span>
-                            {report.intern}
-                        </p>
-                        <p className="flex gap-2">
-                            <span className="font-medium">Stack:</span>
-                            {report.stack}
-                        </p>
-                        {report.type === "activity" && (
-                            <p className="flex gap-2">
-                                <span className="font-medium">Duration:</span>
-                                {report.period}
-                            </p>
-                        )}
-                    </div>
-
-                    <button
-                        className="cursor-pointer rounded-full bg-navBg2 px-4 py-1 font-medium capitalize text-white hover:scale-105 w-fit"
-                        onClick={() => setShowPopup(true)}
-                    >give feeback</button>
-                </div>
-                {showPopup && (
-                    <Feedback
-                        onSend={handleSendFeedback}
-                        onClose={() => setShowPopup(false)}
-                    />
-                )}
-
-                {report.type === "activity" && (
-                    <div className="mt-4 space-y-5">
-
-                        <ListDisplay Text="Completed Tasks" content={report.completed} />
-                        <ListDisplay Text="Ongoing Tasks" content={report.ongoing} />
-                        <ListDisplay Text={ `Tasks for next ${report.period.slice(0, -2)}`} content={report.nextTask} />
-
-                    </div>
-                )}
-
-
-                {report.type === "project" && (
-                    <div className="mt-4">
-                        <h2 className="text-2xl font-semibold capitalize">{report.title} Report</h2>
-                        <p className="mt-2 text-gray-700 text-base p-4  rounded-md whitespace-normal w-full break-words">
-                            {report.Summary}
-                        </p>
-                    </div>
-                )}
-                {/* add submissions ceejay */}
-            </div>
+          <button
+            className="w-fit cursor-pointer rounded-full bg-navBg2 px-4 py-1 font-medium capitalize text-white hover:scale-105"
+            onClick={() => setShowPopup(true)}
+          >
+            give feeback
+          </button>
         </div>
-    )
-}
+        {showPopup && (
+          <Feedback
+            onSend={handleSendFeedback}
+            onClose={() => setShowPopup(false)}
+          />
+        )}
 
-export default ViewReport
+        {report.report_type === "activity" && (
+          <div className="mt-4 space-y-5">
+            <ListDisplay Text="Completed Tasks" content={report.completed} />
+            <ListDisplay Text="Ongoing Tasks" content={report.ongoing} />
+            <ListDisplay
+              Text={`Tasks for next ${report.duration.slice(0, -2)}`}
+              content={report.next}
+            />
+          </div>
+        )}
+
+        {report.report_type === "project" && (
+          <div className="mt-4">
+            <h2 className="text-xl font-semibold capitalize">
+              {report.title} Report
+            </h2>
+            <p className="mt-2 w-full whitespace-normal break-words rounded-md p-4 text-base text-gray-700">
+              {report.summary}
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ViewReport;
