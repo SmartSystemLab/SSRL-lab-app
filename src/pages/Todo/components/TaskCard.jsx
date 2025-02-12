7acb8de64f638c6f8f639563729ccd43b75ae6b8
dabirax <oliverpraise1@gmail.com>
2025-02-12 03:19:40 +0100

    todo 3

--- src/pages/Todo/components/TaskCard.jsx
+++ src/pages/Todo/components/TaskCard.jsx
@@ -2,67 +2,60 @@ import React, { useState } from "react";
 import TaskLabel from "./TaskLabel";
 import { Edit, Trash2 } from "lucide-react";
 
-const TaskCard = ({ name, id, tasks, task, setTasks }) => {
-  const [status, setStatus] = useState(false);
+const TaskCard = ({ tasks, tasky, setTasks }) => {
+  const [task, setTask] = useState(tasky);
+  const { id, name } = task;
+  const [checked, setChecked] = useState(false);
   const [edit, setEdit] = useState(false);
-  const [check, setCheck] = useState(false);
-
-  const handleStatus = (e) => {
-    setStatus(e.target.checked);
-  };
 
   const handleEdit = () => {
     setEdit(!edit);
-    setCheck(!check);
   };
 
-  const deleteTask = (id) => {
-    const updatedTasks = tasks.filter((task) => task.id !== id);
-    setTasks(updatedTasks);
-  };
-
-  const handleChange = (e) => {
-    // setTasks((prevTask) => ({ ...prevTask, [e.name]: name }));
-    console.log(e.name);
-  };
   return (
     <>
-      <div className="item-center m-2 flex justify-between" key={id}>
-        <div className={`item-center flex justify-center gap-4 ${edit && "translate-x-9"} transition-transform duration-500 ease-in-out`}>
-          {!edit && <input
-            type="checkbox"
-            name=""
-            checked={status}
-            id={id}
-            className="w-5"
-            onChange={handleStatus}
-          />}
+      <div
+        className={`item-center m-2 flex items-center justify-between rounded-md border border-transparent px-2 hover:border-zinc-200 hover:shadow-lg`}
+        key={id}
+      >
+        <div
+          className={`item-center flex w-full justify-start gap-4 px-2 py-1 transition-transform duration-500 ease-in-out ${!edit ? "fromRight" : "fromLeft"}`}
+        >
+          {!edit && (
+            <input
+              type="checkbox"
+              name=""
+              checked={checked}
+              id={id}
+              className="w-5"
+              onChange={(e) => setChecked(e.target.checked)}
+            />
+          )}
+
           <TaskLabel
             htmlFor={id}
-            defaultVal={name}
-            onChange={() => {
-              handleChange(task);
-            }}
+            value={name}
+            onChange={(e) => setTask({ ...task, name: e.target.value })}
             placeholder="Enter task"
             required={true}
-            status={status}
+            checked={checked}
             edit={edit}
             handleEdit={handleEdit}
           />
         </div>
-        <div className="flex item-center justify-center gap-2 pt-2">
+        <div className="item-center flex justify-center gap-2">
           {/* Edit */}
-          {status || check || (
+          {checked || edit || (
             <Edit onClick={handleEdit} className="hover:scale-110" />
           )}
           {/* delete */}
-            <Trash2
-              onClick={() => {
-                deleteTask(task.id);
-              }}
-              color="red"
-              className="hover:scale-110"
-            />
+          <Trash2
+            onClick={() => {
+              setTasks(tasks.filter((task) => task.id != id));
+            }}
+            color="red"
+            className="hover:scale-110"
+          />
         </div>
       </div>
     </>
