dec58997c3beecb63a04e3d1629c2cfb91611648
dabirax <oliverpraise1@gmail.com>
2025-05-15 12:13:21 +0100

    pagination3

--- src/pages/Attendance/Pagination.jsx
+++ src/pages/Attendance/Pagination.jsx
@@ -8,21 +8,23 @@ const Pagination = ({
   currentPage,
 }) => {
   let pages = [];
-  const noOfPages = Math.ceil(totalPosts / postsPerPage);
+    // const noOfPages = Math.ceil(totalPosts / postsPerPage);
+  const noOfPages = 5;
   if (noOfPages > 1) {
-    for (let i = 2; i <= noOfPages - 1; i++) {
+    for (let i = 1; i <= noOfPages - 1; i++) {
       pages.push(i);
     }
   }
-  if (currentPage >= 4 && currentPage <= noOfPages - 3) {
+  if (noOfPages>5){
+  if(currentPage==1){
+    pages=[1,2,3]
+  }
+  if (1 < currentPage && currentPage < noOfPages - 3) {
     pages = [currentPage - 1, currentPage, currentPage + 1];
   }
-  if (currentPage < 4) {
-    pages = [2, 3, 4];
-  }
-  if (currentPage > noOfPages - 3) {
-    pages = [noOfPages - 3, noOfPages - 2, noOfPages - 1];
-  }
+  if(currentPage>=noOfPages-3){
+    pages = [noOfPages-4,noOfPages-3,noOfPages-2,noOfPages-1]
+  }}
 
   if (currentPage < 1) {
     setCurrentPage(1);
@@ -32,96 +34,102 @@ const Pagination = ({
   }
 
   return (
-    <div className="flex justify-between">
-      {/* <<< */}
-      <button
-        className="p-2 text-2xl font-semibold"
-        onClick={() => {
-          setCurrentPage(1);
-        }}
-      >
-        &lt;&lt;&lt;
-      </button>
+    <>
+      {noOfPages > 1 ? (
+        <div className="flex justify-between">
+          {/* <<< */}
+          <button
+            className="p-2 text-2xl font-semibold"
+            onClick={() => {
+              setCurrentPage(1);
+            }}
+          >
+            &lt;&lt;&lt;
+          </button>
 
-      {/* < */}
-      <button
-        className="p-2 text-2xl font-semibold"
-        onClick={() => {
-          setCurrentPage(currentPage - 1);
-        }}
-      >
-        &lt;
-      </button>
+          {/* < */}
+          <button
+            className="p-2 text-2xl font-semibold"
+            onClick={() => {
+              setCurrentPage(currentPage - 1);
+            }}
+          >
+            &lt;
+          </button>
 
-      <div className="flex gap-6">
-        {/* 1 */}
-        <button
-          className=""
-          onClick={() => {
-            setCurrentPage(1);
-          }}
-        >
-          1
-        </button>
+          <div className="flex gap-3">
+            {/* pages */}
+            <div className="flex gap-6">
+              {pages.map((page, index) => {
+                return (
+                  <button
+                    key={index}
+                    onClick={() => setCurrentPage(page)}
+                    className={page == currentPage ? "active" : ""}
+                  >
+                    {page}
+                  </button>
+                );
+              })}
+            </div>
 
-        {/* ... */}
-        {currentPage > 3 ? <button>...</button> : ""}
+            <div>
+              {noOfPages > 5 ? (
+                <div>
+                  {/* ... */}
+                  {currentPage < noOfPages - 3 ? (
+                    <button className="mt-2">...</button>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+            {/* last */}
+            <div>
+              {noOfPages > 1 ? (
+                <button
+                  className={
+                    currentPage == noOfPages ? "active mt-1" : "mt-3"
+                  }
+                  onClick={() => {
+                    setCurrentPage(noOfPages);
+                  }}
+                >
+                  {noOfPages}
+                </button>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
 
-        {/* pages */}
-        <div className="flex gap-6">
-          {pages.map((page, index) => {
-            return (
-              <button
-                key={index}
-                onClick={() => setCurrentPage(page)}
-                className={page == currentPage ? "active" : ""}
-              >
-                {page}
-              </button>
-            );
-          })}
+          {/* > */}
+          <button
+            className="p-2 text-2xl font-semibold"
+            onClick={() => {
+              setCurrentPage(currentPage + 1);
+            }}
+          >
+            &gt;
+          </button>
+
+          {/* >>> */}
+          <button
+            className="p-2 text-2xl font-semibold"
+            onClick={() => {
+              setCurrentPage(noOfPages);
+            }}
+          >
+            &gt;&gt;&gt;
+          </button>
         </div>
-
-        {/* ... */}
-        {currentPage <= noOfPages - 3 ? <button>...</button> : ""}
-
-        {/* last */}
-        <div>
-          {noOfPages > 1 ? (
-            <button
-              className="p-3"
-              onClick={() => {
-                setCurrentPage(noOfPages);
-              }}
-            >
-              {noOfPages}
-            </button>
-          ) : (
-            ""
-          )}
-        </div>
-      </div>
-
-      {/* > */}
-      <button
-        className="p-2 text-2xl font-semibold"
-        onClick={() => {
-          setCurrentPage(currentPage + 1);
-        }}
-      >
-        &gt;
-      </button>
-
-      {/* >>> */}
-      <button
-        className="p-2 text-2xl font-semibold"
-        onClick={() => {
-          setCurrentPage(noOfPages);
-        }}
-      >
-        &gt;&gt;&gt;
-      </button>
-    </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
 
